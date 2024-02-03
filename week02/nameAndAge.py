e6e6760d8e680ad19c4814a51b204e7a500fbb1c
Eoghan Walsh <eoghanpwalsh@gmail.com>
2024-02-03 15:55:26 +0000

    name and age

---
+++ week02/nameAndAge.py
@@ -0,0 +1,7 @@
+# nameAndAge.py
+# This program reads in a name and age and prints the output Hello 'name', your age is 'age'.
+# Author: Eoghan Walsh
+
+name = input('Enter your name: ')
+age = int(input('Enter your age: '))
+print(f'Hello {name}, your age is {age}.')
